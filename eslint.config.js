f146ac3fa01aacf98a7a057ee40bc6c3ae813ee5
Emilio Correa <ehcorrea60@gmail.com>
2025-09-16 09:54:11 -0300

    chore(initial commit): adicionado configs para regras commit, regras de lint e setup inicial de test

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'no-console': 'warn',
+    'react/display-name': 'off',
+  },
+};
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  transformIgnorePatterns: [],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 };
