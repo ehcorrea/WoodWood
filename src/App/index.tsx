3f4d026a94429d9ae82828d4605b600b2b88bd4e
Emilio Correa <ehcorrea60@gmail.com>
2025-09-17 10:32:58 -0300

    fix(app): removido texto

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -6,7 +6,7 @@ import Navigation from '../navigators/RootNavigator';
 function App() {
   return (
     <ThemeProvider theme={THEME}>
-      <Navigation />;
+      <Navigation />
     </ThemeProvider>
   );
 }
