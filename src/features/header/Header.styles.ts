8b535d287d502ff479919d2885d6ce6b877e7c5b
Emilio Correa <ehcorrea60@gmail.com>
2025-09-17 18:28:49 -0300

    fix(header): removido height

--- src/features/header/Header.styles.ts
+++ src/features/header/Header.styles.ts
@@ -8,7 +8,6 @@ export const Wrapper = styled.View<WrapperProps>`
   ${({ theme, top }) => css`
     align-items: center;
     background-color: ${theme.colors.primary.main};
-    height: 50%;
     justify-content: center;
     padding: 20px;
     padding-top: ${`${top + 20}px`};
