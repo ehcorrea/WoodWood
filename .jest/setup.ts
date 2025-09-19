c18221c5a814dd5dea95898a7e44f7331e92aeec
Emilio Correa <ehcorrea60@gmail.com>
2025-09-19 09:37:38 -0300

    test(jest): adicionado mock para safe-area

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,3 +1,6 @@
+import { Fragment as MockFragment } from 'react';
+import { View as MockView } from 'react-native';
+
 import '@testing-library/jest-native';
 import { mockedNavigation } from '../src/test/__mocks__';
 
@@ -6,5 +9,14 @@ jest.mock('@react-navigation/native', () => {
   return {
     ...navigation,
     useNavigation: jest.fn(() => mockedNavigation),
+    useRoute: jest.fn(),
   };
 });
+
+jest.mock('react-native-safe-area-context', () => ({
+  SafeAreaProvider: MockFragment,
+  SafeAreaView: MockView,
+  useSafeAreaInsets: () => ({
+    bottom: 10,
+  }),
+}));
